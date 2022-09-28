fd98f2bd46d8eb3c5d49f9d051e84dd57be914aa
Élie Quintard <elie.quintard@gmail.com>
2022-09-28 10:03:30 +0200

    Add Custom Site Plugin

---
+++ public/site/plugins/site/fileMethods.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'customOrientation' => function () {
+       /* if ($this->descriptionSEO()->isNotEmpty()) return $this->descriptionSEO()->smartypants();
+        return $this->site()->descriptionSEO()->smartypants();*/
+    },
+    'imgOptim' => function($class){
+        $image = $this;
+        return "<picture class='".$class."'>
+                 <source srcset='".$image->thumb(['format' => 'webp'])->url()."' type='image/webp'>
+                 <source srcset='".$image->thumb(['format' => 'avif'])->url()."' type='image/avif'>
+                 <img src='".$image->thumb(['format' => 'jpeg'])->url()."' alt='".$image->filename()."'>
+                 </picture>";
+
+    },
+];
---
+++ public/site/plugins/site/siteMethods.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'siteLogo' => function () {
+        if ($this->images()->template("sitelogo")->isNotEmpty()): return "<img class='site-logo' src='" . $this->images()->template("sitelogo") . "'>"; endif;
+    },
+
+    'favicon' => function ($size) {
+        if ($this->images()->template("sitelogo")->isNotEmpty()): $favicon = $this->images()->template("favicon")->first(); endif;
+
+        return $favicon->resize($size)->url();
+    },
+];
