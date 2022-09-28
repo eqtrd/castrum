22e0f182c4d95fce89243e7dc99f5ec136f05bc6
Élie Quintard <elie.quintard@gmail.com>
2022-09-28 14:45:04 +0200

    Update templates

--- public/site/templates/default.php
+++ public/site/templates/default.php
@@ -1,18 +1,7 @@
-<h1><?= $page->title() ?></h1>
+<?php snippet("head")?>
 
-<h1>Malou</h1>
+<h1><?= $page->title()?></h1>
 
-<?php
+<?php snippet("foot")?>
 
-
-use Solaris\MoonPhase;
-
-$moonPhase = new MoonPhase();
-
-$age = round($moonPhase->getAge(), 1);
-$stage = $moonPhase->getPhase() < 0.5 ? 'waxing' : 'waning';
-$distance = round($moonPhase->getDistance(), 2);
-
-echo "The moon is currently " . $age . " days old, and is therefore " . $stage . ". ";
-echo "It is " . $distance . " km from the centre of the Earth. ";
 
---
+++ public/site/templates/home.php
@@ -0,0 +1,5 @@
+<?php snippet("head")?>
+
+<h1>Hello World</h1>
+
+<?php snippet("foot")?>
