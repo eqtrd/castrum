0efea7f77212cc2e2ef19678b0058823a7094c92
Élie Quintard <elie.quintard@gmail.com>
2022-12-20 14:52:37 +0100

    Add font converter

---
+++ .config/fonts.config.js
@@ -0,0 +1,12 @@
+var fontconvert = require('fontconvert-tool');
+const path = require("path");
+
+fontconvert.convertFonts('./src/fonts', './src/fonts', [], {
+
+
+    subset: [
+        'Basic Latin', 'Latin-1 Supplement', // Latin
+        'General Punctuation', 'Currency Symbols' // Punctuation
+    ],
+    outTypes: ['woff', 'woff2'],
+});
