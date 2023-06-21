b92862ad08916c23e46d9230838f5cebd8fd63dc
Élie Quintard <elie.quintard@gmail.com>
2023-06-21 09:36:03 +0200

    Update pageMethods.php

--- public/site/plugins/site/pageMethods.php
+++ public/site/plugins/site/pageMethods.php
@@ -2,22 +2,30 @@
 
 return [
 
-  'createDescription' => function () {
-    if ($this->descriptionSEO()->isNotEmpty()) return $this->descriptionSEO()->smartypants();
-    return $this->site()->descriptionSEO()->smartypants();
-  },
+    'createDescription' => function () {
+        if ($this->descriptionSEO()->isNotEmpty()) return $this->descriptionSEO()->smartypants();
+        return $this->site()->descriptionSEO()->smartypants();
+    },
 
-  'createTitle' => function () {
-      if ($this->template() == 'home'):
-          return $this->site()->title()->smartypants();
-      elseif ($this->template() == 'error'):
-          return $this->site()->title()->smartypants() . ' | This page is not available.';
-      else:
-          return $this->site()->title()->smartypants() . ' | ' . $this->title()->smartypants();
-      endif;
-  },
+    'createTitle' => function () {
+        if ($this->template() == 'home'):
+            return $this->site()->title()->smartypants();
+        elseif ($this->template() == 'error'):
+            return $this->site()->title()->smartypants() . ' | This page is not available.';
+        else:
+            return $this->site()->title()->smartypants() . ' | ' . $this->title()->smartypants();
+        endif;
+    },
 
-  'createImage' => function () {
-      // return $image->crop(1200, 630, 'center')->url();
-  }
+    'createImage' => function () {
+        if ($this->coverImage()->isNotEmpty()) {
+            $image = $this->coverImage()->toFile();
+            return $image->crop(600, 600, 'center')->url();
+        } elseif ($this->images()->isNotEmpty()) {
+            $image = $this->images()->first()->toFile();
+            return $image->crop(600, 600, 'center')->url();
+        }else{
+            return false;
+        }
+    }
 ];
