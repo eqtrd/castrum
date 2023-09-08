4543ad288b2c9a6ca8606664ae0ee283e7274fd6
Élie Quintard <elie.quintard@gmail.com>
2023-09-08 09:56:36 +0200

    Update siteMethods.php

--- public/site/plugins/site/siteMethods.php
+++ public/site/plugins/site/siteMethods.php
@@ -2,7 +2,7 @@
 
 return [
     'siteLogo' => function () {
-        if ($this->images()->template("sitelogo")->isNotEmpty()): return "<img class='site-logo' src='" . $this->images()->template("sitelogo") . "'>"; endif;
+                if ($this->images()->template("sitelogo")->isNotEmpty()): $siteLogo = $this->images()->template("sitelogo")->first(); return "<img class='site-logo' src='" . $siteLogo->url() . "'>"; endif;
     },
 
     'favicon' => function ($size) {
