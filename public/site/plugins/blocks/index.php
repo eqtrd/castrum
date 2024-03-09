d51f13e4e06dbfe21bfbb912d2b38c7f5666975c
Élie Quintard <elie.quintard@gmail.com>
2024-03-09 17:07:44 +0100

    Update .gitignore

--- public/site/plugins/blocks/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-Kirby::plugin('eqtrd/blocks', []);
--- public/site/plugins/site/hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-   /* 'page.update:after' => function ($newPage, $oldPage) {
-        $template = $newPage->coverimage()->toFile()->template();
-        if ($template == ""): $template = "hello"; endif;
-        throw new Exception($newPage->blueprint());
-    },*/
-];
